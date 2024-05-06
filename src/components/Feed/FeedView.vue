3f328b8fc61d19991b5860ed7969e490d8c7b086
lanroo <yladacs@gmail.com>
2024-05-06 15:17:26 -0300

    feed

--- src/components/Feed/FeedView.vue
+++ src/components/Feed/FeedView.vue
@@ -17,20 +17,32 @@
 
     <div class="feed-container">
       <h1>Feed</h1>
+
+      <div class="comment-box">
+        <textarea
+          v-model="newPostContent"
+          placeholder="No que você está pensando?"
+          class="comment-input"
+          @focus="expandTextarea"
+          @input="expandTextarea"
+        ></textarea>
+        <button @click="createPost" class="comment-button">Postar</button>
+      </div>
+
       <div class="post-list">
-        <div v-for="post in posts" :key="post.id" class="post">
+        <div v-for="post in visiblePosts" :key="post.id" class="post">
+          <div class="user-info">
+            <img :src="post.userImage" alt="User Image" class="user-image">
+            <span class="user-name">{{ post.userName }}</span>
+          </div>
           <h2>{{ post.title }}</h2>
           <p>{{ post.content }}</p>
           <img v-if="post.image" :src="post.image" alt="Post Image">
-          <div class="comment-box">
-            <textarea
-              v-model="post.comment"
-              placeholder="Adicione um comentário"
-              class="comment-input"
-              @focus="expandTextarea"
-              @input="expandTextarea"
-            ></textarea>
-            <button @click="addComment(post)" class="comment-button">Comentar</button>
+          <div class="post-actions">
+            <button @click="toggleLike(post)" class="action-button">{{ post.liked ? 'Descurtir' : 'Curtir' }}</button>
+            <button @click="sharePost(post)" class="action-button">Compartilhar</button>
+            <span class="likes-count">{{ post.likes }} Curtidas</span>
+            <span class="shares-count">{{ post.shares }} Compartilhamentos</span>
           </div>
           <div v-if="post.comments && post.comments.length > 0">
             <h3>Comentários:</h3>
@@ -40,6 +52,7 @@
           </div>
         </div>
       </div>
+      <div v-if="loading" class="loading">Carregando...</div>
     </div>
   </div>
 </template>
@@ -51,22 +64,39 @@ export default {
       posts: [
         { 
           id: 1, 
-          title: 'Primeira Postagem', 
-          content: '',
+          userName: 'João Hanz',
+          userImage: 'https://randomuser.me/api/portraits/men/1.jpg',
+          title: 'Nossa nova conquista!',
+          content: 'Hoje alcançamos mais um marco importante em nosso projeto. Estou muito feliz por fazer parte desse time incrível!',
           comment: '', 
-          comments: [] 
+          comments: [],
+          liked: true,
+          likes: 20,
+          shares: 5,
+          image: 'https://via.placeholder.com/150'
         },
         { 
           id: 2, 
-          title: 'Segunda Postagem', 
-          content: 'Conteúdo da segunda postagem.', 
-          image: 'https://via.placeholder.com/150',
+          userName: 'Maria Oliveira',
+          userImage: 'https://randomuser.me/api/portraits/women/2.jpg',
+          title: 'Comemorando aniversário',
+          content: 'É meu aniversário hoje! Muito grata por mais um ano de vida e por todas as pessoas maravilhosas ao meu redor.',
           comment: '', 
-          comments: [] 
+          comments: [],
+          liked: false,
+          likes: 10,
+          shares: 2,
+          image: 'https://via.placeholder.com/150'
         },
-        // Adicione mais postagens conforme necessário
-      ]
+        
+      ],
+      visiblePosts: [],
+      loading: false,
+      newPostContent: ''
     };
+  },
+  created() {
+    this.loadMorePosts();
   },
   methods: {
     goToHome() {
@@ -81,13 +111,49 @@ export default {
     expandTextarea(event) {
       event.target.style.height = 'auto';
       event.target.style.height = event.target.scrollHeight + 'px';
+    },
+    toggleLike(post) {
+      post.liked = !post.liked;
+      post.likes += post.liked ? 1 : -1;
+    },
+    sharePost(post) {
+      post.shares++;
+    },
+    loadMorePosts() {
+      this.loading = true;
+      // Simulação de carregamento de mais posts 
+      setTimeout(() => {
+        const startIndex = this.visiblePosts.length;
+        const endIndex = startIndex + 5;
+        this.visiblePosts = this.posts.slice(0, endIndex);
+        this.loading = false;
+      }, 1000);
+    },
+    createPost() {
+      
+      if (this.newPostContent.trim() !== '') {
+        const newPost = {
+          id: Date.now(),
+          userName: 'Novo Usuário',
+          userImage: 'https://randomuser.me/api/portraits/lego/1.jpg', //random image
+          title: 'Nova Postagem',
+          content: this.newPostContent,
+          comment: '',
+          comments: [],
+          liked: false,
+          likes: 0,
+          shares: 0,
+          image: null 
+        };
+        this.posts.unshift(newPost); 
+        this.newPostContent = ''; 
+      }
     }
   }
 };
 </script>
 
 <style scoped>
-/* Estilo para o componente do feed */
 
 h1 {
   margin-top: 12%;
@@ -103,7 +169,6 @@ h1 {
   border: 1px solid #ccc;
   padding: 20px;
   border-radius: 10px;
-  background: #6a11cb6b;
 }
 
 .post img {
@@ -172,5 +237,52 @@ h1 {
 
 .comment-button:hover {
   background-color: #51279B;
+}
+
+.post-actions {
+  margin-top: 10px;
+}
+
+.action-button {
+  margin-right: 10px;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 20px;
+  background-color: #6A11CB;
+  color: #fff;
+  cursor: pointer;
+  outline: none;
+  transition: background-color 0.3s ease;
+}
+
+.action-button:hover {
+  background-color: #51279B;
+}
+
+.likes-count,
+.shares-count {
+  margin-right: 10px;
+  font-size: 14px;
+  color: #666;
+}
+
+.loading {
+  margin-top: 20px;
+  text-align: center;
+  font-style: italic;
+  color: #666;
+}
+
+.user-info {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.user-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
 }
 </style>
